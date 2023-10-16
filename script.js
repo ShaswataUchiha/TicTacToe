c5059ad4437c1636eb1dd408f1f1e490e6558163
Shaswata Roy <shaswataroy1234@gmail.com>
2023-10-16 20:40:38 +0530

    Update JS File

--- script.js
+++ script.js
@@ -24,6 +24,9 @@ function initGame(){
     boxes.forEach((box, index) => {
         box.innerText = "";
         boxes[index].style.pointerEvents = "all";
+        // Initialize boxes with older css properties
+        box.classList = `box box${index+1}`;
+
     });
 
     newGameButton.classList.remove("active");
@@ -34,8 +37,53 @@ function initGame(){
 // Function Call
 initGame();
 
+// Function to check game result
 function checkGameover(){
-    newGameButton.classList.add("active");
+    let answer = "";
+
+    winningPosition.forEach((position) => {
+        // Check all 3 boxes should not empty and all the vlues are same
+        if( (gameGrid[position[0]] != "" || gameGrid[position[1]] != "" || gameGrid[position[2]] != "" ) 
+        && ( gameGrid[position[0]] === gameGrid[position[1]] ) && ( gameGrid[position[1]] === gameGrid[position[2]] ) ){
+
+            // Checl if winner is X
+            if( gameGrid[position[0]] === "X")
+                answer = "X";
+            else
+                answer = "0";
+
+            // Disable pointer eents
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+            });    
+
+            
+            // now if X or 0 is winner then change it to Green
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+
+        }
+    });
+
+    // Now we have a winner
+    if( answer != ""){
+        playerInfo.innerText = `Winner Player - ${answer}`;
+        newGameButton.classList.add("active");
+        return;
+    }
+
+    // Now if game is tie
+    let boxCount = 0;
+    gameGrid.forEach((box) => {
+        if(box != "")
+            boxCount++;
+    });
+
+    if(boxCount === 9){
+        playerInfo.innerText = `Game Tied !!`;
+        newGameButton.classList.add("active");
+    }
 }
 
 // Player Swap
